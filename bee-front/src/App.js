b6d753211610a30089b13a3a47a0e52b40196df9
Andrianarivo Tantelitiana <arakotoarijaona@ayaline.com>
2022-01-03 13:45:33 +0100

    add new option

--- bee-front/src/App.js
+++ bee-front/src/App.js
@@ -1,6 +1,6 @@
 import {useState,useEffect} from 'react';
 import Post from './components/posts/post';
-import Modal from './components/modal/modal';
+import Modals from './components/modal/modal';
 import axios from 'axios';
 import {Button} from '@material-ui/core';
 import './App.css';
@@ -15,7 +15,32 @@ function App() {
 
   const [opensignIn,setOpensignIn] = useState(false)
   const [opensignUp,setOpensignUp] = useState(false)
+  const [authToken, setAuthToken] = useState(null)
+  const [authTokenType, setAuthTokenType] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [userId, setUserId] = useState('')
+  const [email, setEmail] = useState('')
+  const [newPost, setNewpost] = useState(false)
+  const [image, setImage] = useState(null)
+  const [caption, setCaption] = useState('')
 
+  useEffect(() => {
+    const idUser = localStorage.getItem('userId');
+    const userLogged = localStorage.getItem('username');
+    setAuthToken(localStorage.getItem('authToken'));
+    setAuthTokenType(localStorage.getItem('authTokenType'));
+    setUserId(idUser?idUser:'')
+    setUsername(userLogged?userLogged:'')
+  },[])
+
+  useEffect(()=>{
+    authToken? localStorage.setItem('authToken', authToken): localStorage.removeItem('authToken')
+    authTokenType? localStorage.setItem('authTokenType', authTokenType): localStorage.removeItem('authTokenType')
+    userId? localStorage.setItem('userId', userId): localStorage.removeItem('userId')
+    username? localStorage.setItem('username', username): localStorage.removeItem('username')
+  },
+  [authToken, authTokenType, userId])
   useEffect(()=>{
     const getAllPost = async()=>{
       try{
@@ -34,13 +59,128 @@ function App() {
 
     }
     getAllPost()
+
   }, [])
 
-  const signIn = (event)=>{}
+  async function getConnection(username, password){
+    let formData = new FormData();
+    formData.append('username',username);
+    formData.append('password',password);
+    console.log(username,password)
+    try{
+        const connection = await axios.post('http://localhost:8000/login',formData);
+        setAuthToken(connection.data.access_token);
+        setAuthTokenType(connection.data.token_type);
+        setUserId(connection.data.user_id);
+
+    }catch(error){
+        console.error(error.response.data)
+    }
+  };
+
+  async function getsignUpInformation(username, password, email){
+    let data = {
+      'username':username,
+      'password':password,
+      'email':email
+    };
+
+    try{
+      const options = {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        data,
+        url:'http://localhost:8000/user'
+      }
+      const response = await axios(options);
+      setUsername(response.data.username)
+      setEmail(response.data.email)
+      setPassword(password)
+      signIn()
+    }catch(error){
+      console.error(error.response.data)
+    }
+  }
+
+  async function setNewPost(){
+    const formatData = new FormData();
+    formatData.append('image',image)
+    console.log(image)
+    try{
+      const newImages = await axios.post('http://localhost:8000/post/image',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Autorization': authTokenType+' '+authToken
+        },
+        body:formatData
+      })
+
+      console.log(newImages)
+
+    }catch(error){
+        console.log(error)
+    }
+  }
+
+  const signIn = (event)=>{
+    event?.preventDefault();
+    setOpensignIn(false);
+    getConnection(username, password);
+  }
+
+const signOut =(event)=>{
+  event.preventDefault();
+  setUsername('');
+  setPassword('');
+  setAuthToken(null);
+  setAuthTokenType(null);
+  setOpensignIn(false);
+}
+
+const signUp = (event)=>{
+  event?.preventDefault();
+  setOpensignUp(false);
+  getsignUpInformation(username, password,email);
+}
+
+const handleNewPost = (event)=>{
+  event?.preventDefault();
+  setNewPost();
+
+}
 
   return (
     <div className="app">
-      <Modal isOpen={opensignIn} setIsOpen={setOpensignIn}/>
+      <Modals.ModalSignIn isOpen={opensignIn}
+      setIsOpen={setOpensignIn}
+      signIn={signIn}
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      />
+
+      <Modals.ModalSignUp isOpen={opensignUp}
+      setIsOpen={setOpensignUp}
+      signUp={signUp}
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      email={email}
+      setEmail={setEmail}
+      handleNewPost={handleNewPost}
+      />
+
+      <Modals.ModalNewPost isOpen={newPost}
+      setIsOpen={setNewpost}
+      image={image}
+      setImage={setImage}
+      caption={caption}
+      setCaption={setCaption}
+      />
+
       <div className="app_header">
           <img
           src="https://play-lh.googleusercontent.com/hc3-60fDkOcQlTAgzBbKuh-3EdT8dWax7BroX4zjW8_iKMlOFaE3orm9IUrP7V9zRLw"
@@ -48,8 +188,17 @@ function App() {
           className="app_head_image"
           />
           <div className="app_header_button">
-            <Button onClick={()=>setOpensignIn(true)}>login</Button>
-            <Button onClick={()=>setOpensignUp(true)}>signup</Button>
+            {!(authToken && authTokenType)?
+            <div>
+              <Button onClick={()=>setOpensignIn(true)}>login</Button>
+              <Button onClick={()=>setOpensignUp(true)}>signup</Button>
+            </div>
+          :
+          <div>
+            <Button onClick={()=>setNewpost(true)}>Add Post</Button>
+            <Button onClick={e=>signOut(e)}>logOut</Button>
+          </div>
+          }
           </div>
       </div>
       <div className="app_posts">
--- bee-front/src/components/modal/modal.js
+++ bee-front/src/components/modal/modal.js
@@ -1,11 +1,10 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState,useEffect, useRef} from 'react';
 import {Input, Button} from '@material-ui/core';
 import './modal.css';
 
 
-export default function Modal({isOpen, setIsOpen}) {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+function ModalSignIn({isOpen, setIsOpen, signIn,
+    username, setUsername, password, setPassword}) {
 
     const ref = useRef(null);
 
@@ -21,11 +20,6 @@ export default function Modal({isOpen, setIsOpen}) {
             document.removeEventListener("click", handleClickOutside, true);
         };
     });
-
-    const signIn = (event)=>{
-        event.preventDefault();
-        setIsOpen(false)
-    }
 
     return (
     <div className='modal' style={{display: isOpen ? 'block' : 'none'}}>
@@ -54,3 +48,130 @@ export default function Modal({isOpen, setIsOpen}) {
     </div>
     );
 }
+
+function ModalSignUp({isOpen, setIsOpen, signUp,
+    username, setUsername, password, setPassword, email, setEmail}) {
+
+    const ref = useRef(null);
+    const [passwordMessage, setpasswordMessage] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleClickOutside = event => {
+        if (ref.current && !ref.current.contains(event.target) && isOpen) {
+            setIsOpen(false);
+        }
+    };
+
+    const deactivateMessagePassword = (event)=>{
+        setPassword(event.target.value);
+        setpasswordMessage(false)
+    }
+    const activateMessagePassword = (event)=>{
+        setConfirmPassword(event.target.value);
+        setpasswordMessage(true)
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, true);
+        return () => {
+            document.removeEventListener("click", handleClickOutside, true);
+        };
+    });
+
+    return (
+    <div className='modal' style={{display: isOpen ? 'block' : 'none'}}>
+        <div  className='modal-content' ref={ref}>
+            <form className="app_signin">
+                <center>
+                    <img src="https://play-lh.googleusercontent.com/hc3-60fDkOcQlTAgzBbKuh-3EdT8dWax7BroX4zjW8_iKMlOFaE3orm9IUrP7V9zRLw"
+                    alt="beeApp"
+                    className="app_head_image" />
+                </center>
+                <Input
+                    placeholder="username"
+                    type="text"
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)}
+                />
+                <Input
+                    placeholder="email"
+                    type="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                />
+                <Input
+                    placeholder="password"
+                    type="password"
+                    value={password}
+                    onChange={(e)=>deactivateMessagePassword(e)}
+                    id='Password'
+                />
+                <Input
+                    placeholder="confirm password"
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e)=>activateMessagePassword(e)}
+                />
+                {(password!==confirmPassword && passwordMessage)?<strong style={{color: 'red'}}>password not identical</strong>:<></>}
+                <Button type="submit" onClick={signUp} disabled={!(password===confirmPassword && username && password && email)?true:false}>Submit</Button>
+            </form>
+        </div>
+    </div>
+    );
+}
+
+
+
+function ModalNewPost({isOpen, setIsOpen, image,
+        setImage, caption, setCaption,handleNewPost}) {
+
+    const ref = useRef(null);
+
+    const handleClickOutside = event => {
+        if (ref.current && !ref.current.contains(event.target) && isOpen) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, true);
+        return () => {
+            document.removeEventListener("click", handleClickOutside, true);
+        };
+    });
+
+    const handleChange = event => {
+        if(event.target.files[0]){
+            setImage(event.target.files[0])
+        }
+    }
+
+    return (
+    <div className='modal' style={{display: isOpen ? 'block' : 'none'}}>
+        <div  className='modal-content' ref={ref}>
+            <form className="app_signin">
+                <center>
+                    <img src="https://play-lh.googleusercontent.com/hc3-60fDkOcQlTAgzBbKuh-3EdT8dWax7BroX4zjW8_iKMlOFaE3orm9IUrP7V9zRLw"
+                    alt="beeApp"
+                    className="app_head_image" />
+                </center>
+                <Input
+                    type="file"
+                    id='fileInput'
+                    onChange={e=>handleChange(e)}
+                />
+                <Input
+                    placeholder="Enter a caption"
+                    type="text"
+                    value={caption}
+                    onChange={(e)=>setCaption(e.target.value)}
+                />
+                <Button type="submit" onClick={e=>handleNewPost(e)} disabled={!(image && caption)?true:false}>Upload post</Button>
+            </form>
+        </div>
+    </div>
+    );
+}
+
+const Modals = {ModalSignIn, ModalSignUp, ModalNewPost}
+export default Modals;
